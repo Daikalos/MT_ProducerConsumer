c9d0d632cd11e5526bae73cbec6dd0aea80df30e
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-04-24 11:21:26 +0200

    Project Creation

---
+++ Multithreading_03/Other/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multithreading_03
+{
+    static class Extensions
+    {
+        public static void InvokeIfRequired(this Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
